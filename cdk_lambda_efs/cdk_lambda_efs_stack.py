5aef1a67d1fc8425cd59b542eb03bc82581c9758
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-04-21 11:42:21 +0900

    lambda func

--- cdk_lambda_efs/cdk_lambda_efs_stack.py
+++ cdk_lambda_efs/cdk_lambda_efs_stack.py
@@ -1,7 +1,26 @@
 from aws_cdk import Stack
+from aws_cdk import aws_lambda as _lambda
 from constructs import Construct
 
 
 class CdkLambdaEfsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        self.lambda_func = None
+
+        self.build_infrastructure()
+
+    def build_infrastructure(self):
+        self.build_lambda_func()
+
+    def build_lambda_func(self):
+        _lambda.Function(
+            self,
+            "LambdaWithEFS",
+            runtime=_lambda.Runtime.PYTHON_3_9(),
+            # lambda function file name.handler function
+            handler="lambda_EFS.handler",
+            # Points to directory of lambda function
+            code=_lambda.Code.from_asset("cdk_lambda_efs/lambda_EFS"),
+        )
