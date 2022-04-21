18cb4aff16d41796bc5c9e86d9f7f28725a64680
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-04-21 11:48:33 +0900

    lambda func

--- cdk_lambda_efs/lambda_EFS/lambda_EFS.py
+++ cdk_lambda_efs/lambda_EFS/lambda_EFS.py
@@ -1,2 +1,12 @@
+from pathlib import Path
+
+
 def handler(event, context):
-    return "Hello Lambda!"
+    # Writing to a file on the EFS filesystem
+    path = Path("/mnt/filesystem/test.txt")
+    with path.open("r") as f:
+        f.write("Test123")
+    # Now open the file, read the text, return
+    with path.open("w") as f:
+        text = f.read()
+    return f"Hello Lambda! {text}"
